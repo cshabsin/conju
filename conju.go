0ebe3c6859473e82f2c2bc645f16d3a08fe29b28
Chris Shabsin <cshabsin@gmail.com>
2018-02-10 22:57:32 -0500

    Use native Go templating for Hello, World.

--- conju.go
+++ conju.go
@@ -1,14 +1,22 @@
 package conju
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
 }
 
+var tpl = template.Must(template.ParseGlob("templates/*.html"))
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+	ctx := appengine.NewContext(r)
+	if err := tpl.ExecuteTemplate(w, "test.html", nil); err != nil {
+		log.Errorf(ctx, "%v", err)
+	}
 }
