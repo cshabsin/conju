602fd2c702fe0bc77bdd031f0fe1691ccdb733b9
Chris Shabsin <cshabsin@gmail.com>
2018-02-11 00:38:06 -0500

    Add example of overriding "base" template (test.html)

--- conju.go
+++ conju.go
@@ -16,7 +16,7 @@ var tpl = template.Must(template.ParseGlob("templates/*.html"))
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-	if err := tpl.ExecuteTemplate(w, "test.html", nil); err != nil {
+	if err := tpl.ExecuteTemplate(w, "test2.html", nil); err != nil {
 		log.Errorf(ctx, "%v", err)
 	}
 }
