6813f2cc550a1c28d873871213961d630e2e56fe
Chris Shabsin <cshabsin@gmail.com>
2018-02-12 21:48:45 -0500

    Add event creation for the current event, and a url handler for it.

--- conju.go
+++ conju.go
@@ -1,8 +1,24 @@
 package conju
 
-import "net/http"
+import (
+	"net/http"
+
+	"google.golang.org/appengine"
+)
 
 func init() {
 	http.HandleFunc("/test2", makeTemplateHandler("test.html", "test2.html"))
 	http.HandleFunc("/test3", makeTemplateHandler("test.html", "test3.html"))
+	http.HandleFunc("/create", handleCreate)
+}
+
+func handleCreate(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	err := CreateOneOffEvent(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
 }
--- event.go
+++ event.go
@@ -3,15 +3,32 @@ package conju
 // TODO: move to "package models"?
 
 import (
+	"context"
 	"time"
 
 	"google.golang.org/appengine/datastore"
 )
 
 type Event struct {
-	Name        string
-	StartDate   time.Time
-	EndDate     time.Time
-	PageSnippet *datastore.Key
+	Name      string
+	StartDate time.Time
+	EndDate   time.Time
+	Current   bool
 }
 
+func CurrentEventKey(ctx context.Context) *datastore.Key {
+	// TODO: Get current event from somewhere in datastore instead of
+	// hard-coding it in code.
+	return datastore.NewKey(ctx, "Event", "psr2018", 0, nil)
+}
+
+func CreateOneOffEvent(ctx context.Context) error {
+	e := Event{
+		Name: "Purity Spring 2018",
+		StartDate: time.Date(2018, 6, 8, 0, 0, 0, 0, time.Local),
+		EndDate: time.Date(2018, 6, 11, 0, 0, 0, 0, time.Local),
+		Current: true,
+	}
+	_, err := datastore.Put(ctx, CurrentEventKey(ctx), &e)
+	return err
+}
