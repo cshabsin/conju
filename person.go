3dcd65e938a8cf7b05e4d788f999655eb0ca1b4d
Chris Shabsin <cshabsin@gmail.com>
2018-02-12 21:48:21 -0500

    Add "person" object Add creation code for events, and a URL handler to call it.

---
+++ person.go
@@ -0,0 +1,47 @@
+package conju
+
+// TODO: move to "package models"?
+
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	FirstName   string
+	LastName    string
+	Nickname    string
+	Email       string
+	HomePhone   string
+	MobilePhone string
+	Birthdate   time.Time
+	FallbackAge float64
+}
+
+// FullName returns the formatted full name of the person, with
+// nickname if present.
+func (p *Person) FullName() string {
+	if p.Nickname != "" {
+		return fmt.Sprintf("%s (%s) %s", p.FirstName, p.Nickname, p.LastName)
+	} else {
+		return fmt.Sprintf("%s %s", p.FirstName, p.LastName)
+	}
+}
+
+const (
+	Halfyear time.Duration = 12 * 365 * time.Hour
+	Year                   = 2 * Halfyear
+)
+
+// Returns the age of the person
+func (p Person) ApproxAge() time.Duration {
+	if p.Birthdate.IsZero() {
+		return time.Duration(p.FallbackAge) * Year
+	}
+	return time.Now().Sub(p.Birthdate)
+}
+
+// Round a duration to half-years.
+func HalfYears(d time.Duration) float64 {
+	return d.Round(Halfyear).Hours() / 24 / 365
+}
