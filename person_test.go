9340dd54f09b759b0074d8880626ae22f1cf66ba
Chris Shabsin <cshabsin@gmail.com>
2018-02-13 12:41:24 -0500

    Add full name tests

--- person_test.go
+++ person_test.go
@@ -2,13 +2,14 @@ package conju
 
 import (
 	"fmt"
+	"testing"
 )
 
 func ExampleFirstName() {
 	p := Person{
 		FirstName: "Christopher",
-		Nickname: "Chris",
-		LastName: "Shabsin",
+		Nickname:  "Chris",
+		LastName:  "Shabsin",
 	}
 	fmt.Printf("Informal: %s\n", p.GetFirstName(Informal))
 	fmt.Printf("Formal: %s\n", p.GetFirstName(Formal))
@@ -16,3 +17,24 @@ func ExampleFirstName() {
 	// Informal: Chris
 	// Formal: Christopher
 }
+
+func TestFullNameWithFormality(t *testing.T) {
+	p := Person{
+		FirstName: "Christopher",
+		Nickname:  "Chris",
+		LastName:  "Shabsin",
+	}
+	fn := p.FullNameWithFormality(Full)
+	if fn != "Christopher (Chris) Shabsin" {
+		t.Errorf("Full name was incorrect, got: %s, want: %s.", fn, "Christopher (Chris) Shabsin")
+	}
+	fn = p.FullNameWithFormality(Formal)
+	if fn != "Christopher Shabsin" {
+		t.Errorf("Formal name was incorrect, got: %s, want: %s.", fn, "Christopher Shabsin")
+	}
+	fn = p.FullNameWithFormality(Informal)
+	if fn != "Chris Shabsin" {
+		t.Errorf("Formal name was incorrect, got: %s, want: %s.", fn, "Chris Shabsin")
+	}
+}
+
