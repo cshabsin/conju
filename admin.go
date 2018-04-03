e7c64bd35b2036aaea6cfa4a7fbce3f937d5d3af
Chris Shabsin <cshabsin@gmail.com>
2018-04-02 22:10:24 -0400

    Add IsAdminUser helper function to WrappedRequest

--- admin.go
+++ admin.go
@@ -12,15 +12,15 @@ type AdminInfo struct {
 
 // TODO(cshabsin): Replace error pages with templates.
 func AdminGetter(wr *WrappedRequest) error {
-	u := wr.AdminInfo.User
+	if wr.IsAdminUser() {
+		return nil
+	}
+	u := wr.User
 	if u == nil {
 		url, _ := user.LoginURL(wr.Context, wr.URL.RequestURI())
 		wr.ResponseWriter.Header().Set("Content-Type", "text/html; charset=utf-8")
 		fmt.Fprintf(wr.ResponseWriter, `This page requires administrator access. Please <a href="%s">Sign in</a>.`, url)
 		return DoneProcessingError{}
-	}
-	if u.Admin {
-		return nil
 	}
 	logout_url, err := user.LogoutURL(wr.Context, wr.URL.RequestURI())
 	if err != nil {
