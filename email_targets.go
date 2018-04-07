c766645b3e16184e2b62bb893efae7825ebfbedf
Chris Shabsin <cshabsin@gmail.com>
2018-04-07 00:28:02 -0400

    Implement AllInvitees distributors in email.

--- email_targets.go
+++ email_targets.go
@@ -3,6 +3,8 @@ package conju
 import (
 	"context"
 	"fmt"
+
+	"google.golang.org/appengine/datastore"
 )
 
 // This file defines a set of EmailDistributors, which the
@@ -37,13 +39,65 @@ func SelfOnlyDistributor(wr WrappedRequest, sender EmailSender) error {
 func AllInviteesDryRunDistributor(wr WrappedRequest, sender EmailSender) error {
 	wr.ResponseWriter.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(wr.ResponseWriter, "Looking up all invitees...<br>")
-	// TODO: for all invitees in wr, call sender on the data built up from
-	// that invitee, but with "dry run" values for recipients.
+
+	q := datastore.NewQuery("Invitation").Filter("Event =", wr.EventKey)
+	var invitations []*Invitation
+	invitationKeys, err := q.GetAll(wr.Context, &invitations)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < len(invitations); i++ {
+		realizedInvitation := makeRealizedInvitation(wr.Context, *invitationKeys[i],
+			*invitations[i])
+		for _, p := range realizedInvitation.Invitees {
+			if p.Person.Email == "" {
+				continue
+			}
+			emailData := map[string]interface{}{
+				"Event":      wr.Event,
+				"Invitation": realizedInvitation,
+				"Person":     &p.Person,
+			}
+			fmt.Fprintf(wr.ResponseWriter, "Sending email for %s to %s.<br>", p.Person.Email, wr.LoginInfo.Person.Email)
+			err := sender(wr.Context, emailData, MailHeaderInfo{To: []string{wr.LoginInfo.Person.Email}})
+			if err != nil {
+				fmt.Fprintf(wr.ResponseWriter, "Error emailing %s: %v", p.Person.Email, err)
+				return err
+			}
+		}
+	}
 	return nil
 }
 
 func AllInviteesDistributor(wr WrappedRequest, sender EmailSender) error {
-	// TODO: for all invitees in wr, call sender on the data built up
-	// from that invitee.
+	wr.ResponseWriter.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(wr.ResponseWriter, "Looking up all invitees...<br>")
+
+	q := datastore.NewQuery("Invitation").Filter("Event =", wr.EventKey)
+	var invitations []*Invitation
+	invitationKeys, err := q.GetAll(wr.Context, &invitations)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < len(invitations); i++ {
+		realizedInvitation := makeRealizedInvitation(wr.Context, *invitationKeys[i],
+			*invitations[i])
+		for _, p := range realizedInvitation.Invitees {
+			if p.Person.Email == "" {
+				continue
+			}
+			emailData := map[string]interface{}{
+				"Event":      wr.Event,
+				"Invitation": realizedInvitation,
+				"Person":     &p.Person,
+			}
+			fmt.Fprintf(wr.ResponseWriter, "Sending email for %s.<br>", p.Person.Email)
+			err := sender(wr.Context, emailData, MailHeaderInfo{To: []string{p.Person.Email}})
+			if err != nil {
+				fmt.Fprintf(wr.ResponseWriter, "Error emailing %s: %v", p.Person.Email, err)
+				return err
+			}
+		}
+	}
 	return nil
 }
