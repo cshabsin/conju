aa283c6d7123cc9205a455a2de6a02d62eab1058
Chris Shabsin <cshabsin@gmail.com>
2018-04-07 22:43:21 -0400

    Fixed random login code test

--- login_test.go
+++ login_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestRandomLoginCode(t *testing.T) {
 	for k := 0; k < 100; k++ {
-		lc := randomLoginCode()
+		lc := randomLoginCodeString()
 		for i := 0; i < len(lc); i++ {
 			r := lc[i]
 			if r < 48 || (r > 57 && r < 65) || r > 90 {
