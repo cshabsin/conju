10f84d1be6f0d293590cfe4d44a454c67ef6fa09
dscott617 <dscott@alum.mit.edu>
2018-05-16 22:20:57 -0400

    Help for data cleanup.

--- realized_invitation.go
+++ realized_invitation.go
@@ -29,6 +29,7 @@ type RealizedInvitation struct {
 	LastUpdatedPerson         PersonWithKey
 	LastUpdatedTimestamp      time.Time
 	InviteePeople             []Person
+	FixMeFlag                 bool
 }
 
 func makeRealizedInvitation(ctx context.Context, invitationKey datastore.Key, invitation Invitation) RealizedInvitation {
@@ -124,6 +125,7 @@ func makeRealizedInvitation(ctx context.Context, invitationKey datastore.Key, in
 		OtherInfo:                 invitation.OtherInfo,
 		LastUpdatedPerson:         lastUpdatedPerson,
 		LastUpdatedTimestamp:      invitation.LastUpdatedTimestamp,
+		FixMeFlag:                 (invitation.HasHousingPreference(ShareBed) != invitation.HasHousingPreference(FartherBuilding)),
 	}
 
 	return realizedInvitation
