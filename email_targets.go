883a998061e4746cd8245cf5e3dfb8276a4d1a35
Chris Shabsin <cshabsin@gmail.com>
2019-05-23 21:30:58 -0400

    Add full attendee email distributors

--- email_targets.go
+++ email_targets.go
@@ -22,7 +22,9 @@ var AllDistributors = map[string]EmailDistributorEntry{
 	"SelfOnly":          {false, SelfOnlyDistributor},
 	"AllInviteesDryRun": {false, AllInviteesDryRunDistributor},
 	"AllInvitees*REAL*": {true, AllInviteesDistributor},
+	"AttendeesList":     {false, AttendeesListDistributor},
 	"AttendeesDryRun":   {false, AttendeesDryRunDistributor},
+	"Attendees*REAL*":   {true, AttendeesDistributor},
 }
 
 func SelfOnlyDistributor(wr WrappedRequest, sender EmailSender) error {
@@ -110,6 +112,36 @@ func AllInviteesDistributor(wr WrappedRequest, sender EmailSender) error {
 	return nil
 }
 
+func AttendeesListDistributor(wr WrappedRequest, sender EmailSender) error {
+	wr.ResponseWriter.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(wr.ResponseWriter, "Looking up all attendees...<br>")
+
+	q := datastore.NewQuery("Invitation").Filter("Event =", wr.EventKey)
+	var invitations []*Invitation
+	invitationKeys, err := q.GetAll(wr.Context, &invitations)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < len(invitations); i++ {
+		realizedInvitation := makeRealizedInvitation(wr.Context, *invitationKeys[i],
+			*invitations[i])
+		roomingInfo := getRoomingInfoWithInvitation(wr, invitations[i], invitationKeys[i])
+		if roomingInfo == nil {
+			continue
+		}
+		for _, p := range realizedInvitation.Invitees {
+			if p.Person.Email == "" {
+				continue
+			}
+			if _, found := roomingInfo.Attendees[p.Person.DatastoreKey.IntID()]; !found {
+				continue
+			}
+			fmt.Fprintf(wr.ResponseWriter, "Would send email for %s to %s.<br>", p.Person.Email, wr.LoginInfo.Person.Email)
+		}
+	}
+	return nil
+}
+
 func AttendeesDryRunDistributor(wr WrappedRequest, sender EmailSender) error {
 	wr.ResponseWriter.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(wr.ResponseWriter, "Looking up all attendees...<br>")
@@ -134,18 +166,59 @@ func AttendeesDryRunDistributor(wr WrappedRequest, sender EmailSender) error {
 			if _, found := roomingInfo.Attendees[p.Person.DatastoreKey.IntID()]; !found {
 				continue
 			}
-			// emailData := map[string]interface{}{
-			// 	"Event":       wr.Event,
-			// 	"Invitation":  realizedInvitation,
-			// 	"Person":      &p.Person,
-			// 	"RoomingInfo": roomingInfo,
-			// }
+			emailData := map[string]interface{}{
+				"Event":       wr.Event,
+				"Invitation":  realizedInvitation,
+				"Person":      &p.Person,
+				"RoomingInfo": roomingInfo,
+			}
 			fmt.Fprintf(wr.ResponseWriter, "Sending email for %s to %s.<br>", p.Person.Email, wr.LoginInfo.Person.Email)
-			// err := sender(wr.Context, emailData, MailHeaderInfo{To: []string{wr.LoginInfo.Person.Email}})
-			// if err != nil {
-			// 	fmt.Fprintf(wr.ResponseWriter, "Error emailing %s: %v", p.Person.Email, err)
-			// 	return err
-			// }
+			err := sender(wr.Context, emailData, MailHeaderInfo{To: []string{wr.LoginInfo.Person.Email}})
+			if err != nil {
+				fmt.Fprintf(wr.ResponseWriter, "Error emailing %s: %v", p.Person.Email, err)
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func AttendeesDistributor(wr WrappedRequest, sender EmailSender) error {
+	wr.ResponseWriter.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(wr.ResponseWriter, "Looking up all attendees...<br>")
+
+	q := datastore.NewQuery("Invitation").Filter("Event =", wr.EventKey)
+	var invitations []*Invitation
+	invitationKeys, err := q.GetAll(wr.Context, &invitations)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < len(invitations); i++ {
+		realizedInvitation := makeRealizedInvitation(wr.Context, *invitationKeys[i],
+			*invitations[i])
+		roomingInfo := getRoomingInfoWithInvitation(wr, invitations[i], invitationKeys[i])
+		if roomingInfo == nil {
+			continue
+		}
+		for _, p := range realizedInvitation.Invitees {
+			if p.Person.Email == "" {
+				continue
+			}
+			if _, found := roomingInfo.Attendees[p.Person.DatastoreKey.IntID()]; !found {
+				continue
+			}
+			emailData := map[string]interface{}{
+				"Event":       wr.Event,
+				"Invitation":  realizedInvitation,
+				"Person":      &p.Person,
+				"RoomingInfo": roomingInfo,
+			}
+			fmt.Fprintf(wr.ResponseWriter, "Sending email for %s.<br>", p.Person.Email)
+			err := sender(wr.Context, emailData, MailHeaderInfo{To: []string{p.Person.Email}})
+			if err != nil {
+				fmt.Fprintf(wr.ResponseWriter, "Error emailing %s: %v", p.Person.Email, err)
+				return err
+			}
 		}
 	}
 	return nil
