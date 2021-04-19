ce1809dd110a07519b388eec06a4ec430f5fef4e
Chris Shabsin <cshabsin@gmail.com>
2021-04-19 17:26:01 -0400

    Make the poll invitation-based, not person-based

--- model/poll/poll.go
+++ model/poll/poll.go
@@ -11,17 +11,17 @@ import (
 // Answer defines an answer to the one question we have, "how crazy is this?" (from PSR 2021).
 // Should be generalized someday.
 type Answer struct {
-	Person   *datastore.Key
-	Question int // always 1 for now
-	Rating   int
+	Invitation *datastore.Key
+	Question   int // always 1 for now
+	Rating     int
 }
 
-// GetAnswer returns the key and data for a person's answer to the poll. Returns nil if the
+// GetAnswer returns the key and data for an invitation's answer to the poll. Returns nil if the
 // user hasn't answered yet.
-func GetAnswer(ctx context.Context, personKey *datastore.Key) (*datastore.Key, *Answer, error) {
-	q := datastore.NewQuery("Answer").Filter("Person=", personKey).Filter("Question=", 1)
+func GetAnswer(ctx context.Context, invitationKey *datastore.Key) (*datastore.Key, *Answer, error) {
+	q := datastore.NewQuery("Answer").Filter("Invitation=", invitationKey).Filter("Question=", 1)
 	if q == nil {
-		return nil, nil, fmt.Errorf("nil query for person %q", personKey.String())
+		return nil, nil, fmt.Errorf("nil query for invitation %q", invitationKey.String())
 	}
 	var answers []*Answer
 	keys, err := q.GetAll(ctx, &answers)
@@ -32,18 +32,18 @@ func GetAnswer(ctx context.Context, personKey *datastore.Key) (*datastore.Key, *
 		return nil, nil, nil
 	}
 	if len(keys) > 1 {
-		return nil, nil, fmt.Errorf("more than one poll response for person %q", personKey.String())
+		return nil, nil, fmt.Errorf("more than one poll response for invitation %q", invitationKey.String())
 	}
 	return keys[0], answers[0], nil
 }
 
-// SetAnswer sets a person's poll answer. If the caller has access to the answerKey from a previous
+// SetAnswer sets an invitation's poll answer. If the caller has access to the answerKey from a previous
 // GetAnswer call this can be done more efficiently.
-func SetAnswer(ctx context.Context, personKey *datastore.Key, answerKey *datastore.Key, rating int) (*datastore.Key, error) {
+func SetAnswer(ctx context.Context, invitationKey *datastore.Key, answerKey *datastore.Key, rating int) (*datastore.Key, error) {
 	answer := &Answer{
-		Person:   personKey,
-		Question: 1,
-		Rating:   rating,
+		Invitation: invitationKey,
+		Question:   1,
+		Rating:     rating,
 	}
 	if answerKey != nil {
 		return datastore.Put(ctx, answerKey, answer)
