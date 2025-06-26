4710eaecd2c2cf68eba29184216f085e403ac714
Chris Shabsin <cshabsin@gmail.com>
2025-06-26 09:43:20 -0400

    update pricing estimate for 2025

--- media/pricing_estimate.js
+++ media/pricing_estimate.js
@@ -1,6 +1,6 @@
 
-const babyLimitDate = new Date(2020, 8, 30);
-const kidLimitDate = new Date(2012, 8, 30);
+const babyLimitDate = new Date(2021, 8, 30);
+const kidLimitDate = new Date(2013, 8, 30);
 
 
 function computeCost() {
@@ -35,9 +35,27 @@ function computeCostWithRoommates(additionalPeople) {
     numberOfNights = 3;
   }
 
+    // Room rate:  Carrigain 290  O/T 255  KP 210
+    // If only one cost class is selected use that
+    // If more than one selected, use O/T if selected.
+    // If CR and KP selected choose KP
 
-  var roomRate = 300;
-  if ($('input[name="housingPreferenceBooleans"][value="7"]').is(":checked")) roomRate = 390;
+  var roomRate = 255;
+  var roomRateDescription = "Default room rate of $255 per night";
+    // this was prefer more expensive room I think
+    //if ($('input[name="housingPreferenceBooleans"][value="7"]').is(":checked")) roomRate = 390;
+  if ($('input[name="housingPreferenceBooleans"][value="1"]').is(":checked")) {
+    roomRate = 210;
+    roomRateDescription = "Selected King Pine room rate of $210 per night";
+  }
+  if ($('input[name="housingPreferenceBooleans"][value="0"]').is(":checked")) {
+    roomRate = 255;
+    roomRateDescription = "Selected Osceola/Tecumseh room rate of $255 per night";
+  }
+  if ($('input[name="housingPreferenceBooleans"][value="2"]').is(":checked")) {
+    roomRate = 290;
+    roomRateDescription = "Selected Carrigain room rate of $290 per night";
+  }
 
   var rsvps = $('select[name="rsvp"]').map(function() {return $(this).val()}).toArray();
   var totalAttendees = 0;
@@ -60,14 +78,16 @@ function computeCostWithRoommates(additionalPeople) {
 
   var adultFridayDinnerCost = 15;
   var kidFridayDinnerCost = 10;
-  var adultMealCost = 35;
-  var kidMealCost = 20;
-  var babyFoodCost = 0;
+  var adultMealCost = 50;
+  var adultCateredCost = 10;    
+  var kidMealCost = 25;
+  var kidCateredCost = 5;
+  // var babyFoodCost = 0;
   
   const pricesByAgeAndNights = [
-      [adultFridayDinnerCost + adultMealCost * 5, adultFridayDinnerCost + adultMealCost * 3, adultMealCost * 5],
-      [kidFridayDinnerCost + kidMealCost * 5, kidFridayDinnerCost + kidMealCost * 3, kidMealCost * 5],
-      [0, 0, 0]
+      [adultFridayDinnerCost + adultMealCost * 3 + adultCateredCost * 2, adultFridayDinnerCost + adultMealCost * 2 + adultCateredCost * 2],
+      [kidFridayDinnerCost + kidMealCost * 3 + kidCateredCost * 2, kidFridayDinnerCost + kidMealCost * 2 + kidCateredCost * 2],
+      [0, 0]
   ]
 
   var birthdates = $('input[name="Birthdate"]').map(function() {return $(this).val()}).toArray();
@@ -77,7 +97,7 @@ function computeCostWithRoommates(additionalPeople) {
     var nightsIndex = -1;
     if (rsvps[i] == "5") nightsIndex = 0;
     else if (rsvps[i] == "2") nightsIndex = 1;
-    else if (rsvps[i] == "4") nightsIndex = 2;
+//    else if (rsvps[i] == "4") nightsIndex = 2;
     else continue;
 
     var ageIndex = 0;
@@ -95,7 +115,7 @@ function computeCostWithRoommates(additionalPeople) {
 
   //console.log("food cost: " + foodCost); 
 
-    var incidentalsPerPersonPerNight = 20; // includes bulk covid tests
+    var incidentalsPerPersonPerNight = 5; // assumes $800 of expenses, 50 people staying 3 nights, 20 people staying 2 nights, no covid tests
  
   var incidentalsCost = incidentalsPerPersonPerNight *  $('select[name="rsvp"] option:selected').map(function() {
     if ($( this ).val() == "5") return 3;
@@ -105,12 +125,11 @@ function computeCostWithRoommates(additionalPeople) {
       //console.log("incidentals cost: " + incidentalsCost);
 
   var totalCost = lodgingCost + foodCost + incidentalsCost;
-  $(".estimatedCost").text("$" + totalCost)
+  $(".estimatedCost").text("$" + totalCost + " (" + roomRateDescription + ")");
 
       //console.log(totalCost);
   //alert(totalCost);
   return Math.ceil(totalCost);
 
 }
-
 
