a58d86ffca33efab7f3aaeac7ca6175068345ff1
Chris Shabsin <cshabsin@gmail.com>
2025-06-27 23:30:06 -0400

    Fix crashing /info handler

--- conju/info.go
+++ conju/info.go
@@ -2,11 +2,13 @@ package conju
 
 import (
 	"context"
+	"fmt"
 	"html/template"
 	"log"
 )
 
 func handleInfo(ctx context.Context, wr *WrappedRequest) {
+	wr.ResponseWriter.Header().Set("Content-Type", "text/html")
 	eventName := "PSR2022"
 	if wr.Event != nil {
 		eventName = wr.Event.ShortName
@@ -14,6 +16,8 @@ func handleInfo(ctx context.Context, wr *WrappedRequest) {
 	tpl, err := template.ParseFiles("templates/main.html", "templates/"+eventName+"/info.html")
 	if err != nil {
 		log.Println("info ParseFiles", err)
+		fmt.Fprintf(wr.ResponseWriter, "Error: %v<p>", err)
+		return
 	}
 	if err := tpl.ExecuteTemplate(wr.ResponseWriter, "info.html", wr.TemplateData); err != nil {
 		log.Println("info ExecuteTemplate", err)
